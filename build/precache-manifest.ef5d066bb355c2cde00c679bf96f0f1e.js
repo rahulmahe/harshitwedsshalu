3d37e84c792271931de01495bd835132d15af649
Rahul Maheshwari <rahul.maheshwari@izenda.com>
2019-11-17 15:29:51 +0530

    Start with create-react-app

---
+++ build/precache-manifest.ef5d066bb355c2cde00c679bf96f0f1e.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c5a98a9870f31661606660c91eb376e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5151a8d9df9f1fba3644",
+    "url": "/static/css/2.2a18363d.chunk.css"
+  },
+  {
+    "revision": "550d65591da82ca23189",
+    "url": "/static/css/main.46a6907e.chunk.css"
+  },
+  {
+    "revision": "5151a8d9df9f1fba3644",
+    "url": "/static/js/2.f9375eb5.chunk.js"
+  },
+  {
+    "revision": "550d65591da82ca23189",
+    "url": "/static/js/main.1466c41f.chunk.js"
+  },
+  {
+    "revision": "02bea16878f30492a9e7",
+    "url": "/static/js/runtime-main.641e2c7b.js"
+  },
+  {
+    "revision": "4b47848e5469613c7e860305793017c5",
+    "url": "/static/media/1.4b47848e.jpeg"
+  },
+  {
+    "revision": "f8a3798a5c0ad3ae7f4efd4caab0335c",
+    "url": "/static/media/Banner-2.f8a3798a.jpg"
+  },
+  {
+    "revision": "0af76f9126c5565ce1089784f6a6a04b",
+    "url": "/static/media/Banner-3.0af76f91.jpg"
+  },
+  {
+    "revision": "06a04537db9294681f93bc12bd1d9c53",
+    "url": "/static/media/OpenSans-Regular.06a04537.eot"
+  },
+  {
+    "revision": "629a55a7e793da068dc580d184cc0e31",
+    "url": "/static/media/OpenSans-Regular.629a55a7.ttf"
+  },
+  {
+    "revision": "854f710da60656a434cd9bc416b7cb5f",
+    "url": "/static/media/OpenSans-Regular.854f710d.woff"
+  },
+  {
+    "revision": "9400310d507c2f9fef9f84fc8d252fef",
+    "url": "/static/media/OpenSans-Regular.9400310d.svg"
+  },
+  {
+    "revision": "3b8fed93f46e41f2a69bd65c289de89b",
+    "url": "/static/media/PIE.3b8fed93.htc"
+  },
+  {
+    "revision": "2d52000bd7b793d05e31254d1dd5d882",
+    "url": "/static/media/Pacifico-webfont.2d52000b.svg"
+  },
+  {
+    "revision": "92330818849350cc91c955d9d6015f77",
+    "url": "/static/media/Pacifico-webfont.92330818.woff"
+  },
+  {
+    "revision": "a5e945ab4a79d0db810c434040945ef2",
+    "url": "/static/media/Pacifico-webfont.a5e945ab.ttf"
+  },
+  {
+    "revision": "ba221906f74da6944bf092f4b34fbcbe",
+    "url": "/static/media/Pacifico-webfont.ba221906.eot"
+  },
+  {
+    "revision": "e241d4a24a173e04f7e10ffa2c678059",
+    "url": "/static/media/Pacifico-webfont.e241d4a2.woff2"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/static/media/slick.f97e3bbf.svg"
+  }
+]);
